69b7286dcbaf37ca0fb653b4fbcfc40169f9b4f1
HellblazerXXL <royceapera@yahoo.com>
2022-09-16 07:50:03 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"programming is building a multilingual puzzle");
+	return (0);
+}
