cc4fda7b0bb1fd41a9084421189e9234afc32f08
HellblazerXXL <royceapera@yahoo.com>
2022-09-16 12:48:57 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	long long alonglong;
 	float afloat;
 
-	printf("size of a char: %lu byte(s)\n", sizeof acharacter));
+	printf("size of a char: %lu byte(s)\n", sizeof (acharacter));
 	printf("size of an int: %lu byte(s)\n", sizeof (ainteger));
 	printf("size of a long int: %lu bytes(s)\n", sizeof (along));
 	printf("size of a long long int: %lu bytes(s)\n", sizeof(alonglong));
