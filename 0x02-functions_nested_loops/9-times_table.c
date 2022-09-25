611dcecd249c40984b83d41351275a6fa1d25b2e
HellblazerXXL <royceapera@yahoo.com>
2022-09-25 04:25:38 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,20 +15,20 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-	mult = a * b;
-if  (mult >= 10)
-{
-_putchar(' ');
-_putchar((mult / 10) + '0');
-_putchar((mult % 10) + '0');
-if (b != 9)
-{
-	_putchar(',');
-}
-}
-else
-{
-	if (b != 9)
+			mult = a * b;
+			if  (mult >= 10)
+		{
+			_putchar(' ');
+			_putchar((mult / 10) + '0');
+			_putchar((mult % 10) + '0');
+			if (b != 9)
+		{
+			_putchar(',');
+		}
+	}
+		else
+	{
+		if (b != 9)
 	{
 		_putchar(' ');
 		_putchar(' ');
